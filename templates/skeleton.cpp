a71b877720902e1a9cda60f019cc1c48c512b36d
Ryu Yamada[alias] <a2sin2a2ko@kyoto-y.work>
2020-08-31 21:27:15 +0900

    add : update include

--- templates/skeleton.cpp
+++ templates/skeleton.cpp
@@ -3,11 +3,6 @@
 #include <map>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
 #include <cmath>
 using namespace std;
 #define ll long long
