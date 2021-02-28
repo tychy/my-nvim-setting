f41f5017c39550e7096cd4c0de6e4d1f1ac08e1a
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-02-28 22:44:10 +0900

    update : macro

--- templates/skeleton.cpp
+++ templates/skeleton.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n);i++)
 #define Graph vector<vector<int>>;
 #define iterG(next_v, G, v) for(auto next_v : G[v]
+#define ALL(a)  (a).begin(),(a).end()
+const int MOD = 1000000007;
+const int MAX = 510000;
+const int INF = 1061109567;
+const double EPS = 1e-10;
+const double PI  = acos(-1.0);
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
